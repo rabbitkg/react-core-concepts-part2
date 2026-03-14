37bdcac96451744d11345082b2acbfad300e60e7
rabbitkg <rabbitkgbd@gmail.com>
2026-03-14 06:17:03 +0600

    more api function import for best practic.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import Counter from './Counter'
 import Batsman from './Batsman'
 import Users from './Users';
+import Friends from './Friends';
+import Posts from './Posts';
 import './App.css'
+import { Suspense } from 'react';
 
+
+// const fetchUsers = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json())
+
+
+// const fetchFriends = async() => {
+//   const res = await fetch('http://jsonplaceholder.typicode.com/users');
+//   return res.json();
+// }
+
+const fetchPosts = async() =>{
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+  return res.json();
+}
 
 function App() {
+
+  // const friendsPromise = fetchFriends();
+  const postsPromise = fetchPosts();
 
   function handleClick(){
     alert('I am clicked.')
@@ -23,6 +42,19 @@ function App() {
     <>
 
           <h3>Vite + React</h3>
+
+          <Suspense fallback={<h4>Post are coming.......... </h4>}>
+            <Posts postsPromise={postsPromise}></Posts>
+          </Suspense>
+
+          {/* <Suspense fallback={<h3>Loading...</h3>}>
+            <Users fetchUsers = {fetchUsers}></Users>
+          </Suspense> */}
+
+
+          {/* <Suspense fallback={<h3>Friends are coming for treat...</h3>}>
+            <Friends friendsPromise={friendsPromise}></Friends>
+          </Suspense> */}
 
           <Users></Users>
           
--- src/Summary.jsx
+++ src/Summary.jsx
@@ -10,12 +10,17 @@
 // 1. API : url : https://jsonplaceholder.typicode.com/users
 
 
-fetch("https://jsonplaceholder.typicode.com/users")
-.then(res => res.json())
-.then(data => console.log(data));
 
-const loadData = async() => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/users');
-    const data = res.json();
-    return data;
-}
+// 1. just write a simple fetch with json conversion.
+// 2. wrap the data loading component under suspense
+
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+// .then(res => res.json())
+// .then(data => console.log(data));
+
+// const loadData = async() => {
+//     const res = await fetch('https://jsonplaceholder.typicode.com/users');
+//     const data = res.json();
+//     return data;
+// }
